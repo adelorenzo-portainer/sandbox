a82f95da8fa88c6b9c24ecf56eba2eecc4565b5f
root <root@portainer.d.lan>
2021-08-18 01:31:46 +0000

    fix for kind

--- select.sh
+++ select.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/ash
 
 response=$(zenity --height=250 --list --checklist \
-   --title='Selection' --column=Boxes --column=Selections \
+   --text='Please select the orchestrator(s)' --column=Check --column=Orchestrator \
    TRUE Docker TRUE Docker-Swarm TRUE Kubernetes --separator=':')
 
 if [ -z "$response" ] ; then
@@ -11,8 +11,8 @@ fi
 
 IFS=":" ; for word in $response ; do 
    case $word in
-      Docker) echo install_standalone.sh ;;
-      Docker-Swarm) echo install_swarm.sh ;;
-      Kubernetes) echo install_kube.sh ;;
+      Docker) /usr/local/bin/.install_standalone.sh | zenity --text "Installing Docker" --progress --auto-close --pulsate ;;
+      Docker-Swarm) echo install_swarm.sh  | zenity --title "Installing Docker Swarm" --progress --auto-close --pulsate ;;
+      Kubernetes) echo install_kube.sh  | zenity --title "Installing Kubernetes" --progress --auto-close --pulsate ;;
    esac
 done
