b273ee6e4c3ebddb6193129f78f2bddfcbf81e40
adelorenzo-portainer <81579885+adelorenzo-portainer@users.noreply.github.com>
2021-09-30 20:43:45 -0600

    Update firstboot.sh

--- firstboot.sh
+++ firstboot.sh
@@ -1,34 +1,36 @@
 #!/bin/ash
 
 #Install Docker in Docker
-echo 10 | dialog --title "Portainer Sandbox Setup" --gauge "Installing Docker" 10 80 0
-/root/install_standalone.sh > /dev/null 2>&1
-sleep 1
+#echo 10 | dialog --title "Portainer Sandbox Setup" --gauge "Installing Docker" 10 80 0
+#/root/install_standalone.sh > /dev/null 2>&1
+#sleep 1
 
 #Install Docker Swarm in Docker
-echo 30 | dialog --title "Portainer Sandbox Setup" --gauge "Installing Docker Swarm" 10 80 0
-/root/install_swarm.sh > /dev/null 2>&1
-sleep 1
+#echo 30 | dialog --title "Portainer Sandbox Setup" --gauge "Installing Docker Swarm" 10 80 0
+#/root/install_swarm.sh > /dev/null 2>&1
+#sleep 1
 
 #Install Minkube
-echo 50 | dialog --title "Portainer Sandbox Setup" --gauge "Installing Kubernetes" 10 80 0
-/root/install_minikube.sh > /dev/null 2>&1
-sleep 1
+#echo 50 | dialog --title "Portainer Sandbox Setup" --gauge "Installing Kubernetes" 10 80 0
+#/root/install_minikube.sh > /dev/null 2>&1
+#sleep 1
+
+/root/install_minikube.sh | dialog --progressbox 30 100
 
 #Add Docker Endpoint
-echo 60 | dialog --title "Portainer Sandbox Setup" --gauge "Adding Docker Endpoint" 10 80 0
-/root/endpoint_standalone.sh > /dev/null 2>&1
-sleep 1
+#echo 60 | dialog --title "Portainer Sandbox Setup" --gauge "Adding Docker Endpoint" 10 80 0
+#/root/endpoint_standalone.sh > /dev/null 2>&1
+#sleep 1
 
 #Add Docker Swarm Endpoint
-echo 70 | dialog --title "Portainer Sandbox Setup" --gauge "Adding Docker Swarm Endpoint" 10 80 0
-/root/endpoint_swarm.sh > /dev/null 2>&1
-sleep 1
+#echo 70 | dialog --title "Portainer Sandbox Setup" --gauge "Adding Docker Swarm Endpoint" 10 80 0
+#/root/endpoint_swarm.sh > /dev/null 2>&1
+#sleep 1
 
 #Add Minikube Endpoint
-echo 80 | dialog --title "Portainer Sandbox Setup" --gauge "Adding Kubernetes Endpoint" 10 80 0
-/root/endpoint_minikube.sh > /dev/null 2>&1
-sleep 1
+#echo 80 | dialog --title "Portainer Sandbox Setup" --gauge "Adding Kubernetes Endpoint" 10 80 0
+#/root/endpoint_minikube.sh > /dev/null 2>&1
+#sleep 1
 
 #Housekeeping
 echo 100 | dialog --title "Portainer Sandbox Setup" --gauge "Final settings..." 10 80 0
