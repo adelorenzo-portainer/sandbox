493bb79c800ae93be5bee6e48c286d00b04d8b0c
adelorenzo-portainer <81579885+adelorenzo-portainer@users.noreply.github.com>
2021-09-30 18:20:40 -0600

    Update install_kube.sh

--- install_kube.sh
+++ install_kube.sh
@@ -211,11 +211,95 @@ spec:
   versionPriority: 100
 EOY
 
+kubectl apply -f - <<EOY
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: portainer
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: portainer-sa-clusteradmin
+  namespace: portainer
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: portainer-crb-clusteradmin
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: portainer-sa-clusteradmin
+  namespace: portainer
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: portainer-agent
+  namespace: portainer
+spec:
+  type: NodePort
+  selector:
+    app: portainer-agent
+  ports:
+    - name: http
+      protocol: TCP
+      port: 9001
+      targetPort: 9001
+      nodePort: 30778
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: portainer-agent-headless
+  namespace: portainer
+spec:
+  clusterIP: None
+  selector:
+    app: portainer-agent
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: portainer-agent
+  namespace: portainer
+spec:
+  selector:
+    matchLabels:
+      app: portainer-agent
+  template:
+    metadata:
+      labels:
+        app: portainer-agent
+    spec:
+      serviceAccountName: portainer-sa-clusteradmin
+      containers:
+      - name: portainer-agent
+        image: portainerci/agent:develop
+        imagePullPolicy: Always
+        env:
+        - name: LOG_LEVEL
+          value: DEBUG
+        - name: AGENT_CLUSTER_ADDR
+          value: "portainer-agent-headless"
+        - name: KUBERNETES_POD_IP
+          valueFrom:
+            fieldRef:
+              fieldPath: status.podIP
+        ports:
+        - containerPort: 9001
+          protocol: TCP
+EOY
+
 # Add Portainer Agent to the local kind Kubernetes cluster
-sleep 5
-wget  -O /tmp/portainer-agent-k8s.yaml https://downloads.portainer.io/portainer-agent-k8s-nodeport.yaml
-kubectl apply -f /tmp/portainer-agent-k8s.yaml --validate=false
-rm /tmp/portainer-agent-k8s.yaml
+# sleep 5
+# wget  -O /tmp/portainer-agent-k8s.yaml https://downloads.portainer.io/portainer-agent-k8s-nodeport.yaml
+# kubectl apply -f /tmp/portainer-agent-k8s.yaml --validate=false
+# rm /tmp/portainer-agent-k8s.yaml
 
 # Check if the Portainer Agent is running
 while true
